75210539dca1ec4f766790cdc6143db5b9a6d1b8
Azganush <43196746+azganushpoghosyan@users.noreply.github.com>
2018-09-12 10:16:36 +0400

    Delete SQLite.query 1.sql

--- SQLite.query 1.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.categoryname AS category, COUNT(*) AS number_of_products, SUM(p.unitsinstock) AS stock
-FROM Categories c
-JOIN Products p
-ON c.categoryid=P.categoryid
-GROUP BY 1
-ORDER BY 3 DESC;
